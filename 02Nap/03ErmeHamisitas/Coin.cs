0a5dafc31bb059f5b953cf9967ee53b24c29d63d
Plesz Gábor <gabor.plesz@gmail.com>
2018-08-02 12:53:10 +0200

    #5: leszármaztatás és függvények: problémafelvetés (érmehamisítás)

---
+++ 02Nap/03ErmeHamisitas/Coin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _03ErmeHamisitas
+{
+    public class Coin
+    {
+        Random random = new Random();
+
+        public int Collect()
+        {
+            return random.Next(0, 2);
+        }
+    }
+}
---
+++ 02Nap/03ErmeHamisitas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03ErmeHamisitas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coin = new Coin();
+
+            Console.WriteLine("Az érmefeldobások eredménye:");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write($"{coin.Collect()},");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
