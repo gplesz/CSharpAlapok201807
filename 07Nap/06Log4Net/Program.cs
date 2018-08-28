dcf6e4c1038255a92737a6a02d2e56d4ac895d87
Plesz Gábor <gabor.plesz@gmail.com>
2018-08-28 12:43:11 +0200

    #10: Naplózás: a programozó oldala

--- 07Nap/06Log4Net/Program.cs
+++ 07Nap/06Log4Net/Program.cs
@@ -9,8 +9,17 @@ namespace _06Log4Net
 {
     class Program
     {
+        //ezen a proerty-n keresztül az adott osztály összes naplóje ezen keresztül megy
+        //http://logging.apache.org/log4net/release/faq.html#static-class-name
+        //érdemes a névben utalni arra az osztályra, aminek a naplójáról van szó.
+        //private static readonly log4net.ILog log = log4net.LogManager.GetLogger("_06Log4Net.Program");
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
+
         static void Main(string[] args)
         {
+            //betöltjük az app.config-ból a log4net konfigurációt
+            log4net.Config.XmlConfigurator.Configure();
+
             var r = new Random();
 
             while (!Console.KeyAvailable) //addig fut, amíg nem ütöttünk le billentyűt
@@ -23,27 +32,27 @@ namespace _06Log4Net
 
                 if (level<50)
                 { //legkisebb súlyú, de legsűrűbben előforduló üzenet (Debug)
-
+                    log.Debug($"Debug üzenet: {level}");
                 }
 
                 if (level>=50 && level<70)
                 { //Info
-
+                    log.Info($"Info üzenet: {level}");
                 }
 
                 if (level>=70 && level<85)
                 { //Warning
-
+                    log.Warn($"Warn üzenet: {level}");
                 }
 
                 if (level>=85 && level<95)
                 { //Error
-
+                    log.Error($"Error üzenet: {level}");
                 }
 
                 if (level>=95)
                 { //Fatal
-
+                    log.Fatal($"Fatal üzenet: {level}");
                 }
                 Thread.Sleep(200);
 
