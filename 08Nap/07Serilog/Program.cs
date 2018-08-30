df0b9be9fc72abe3ddcde80f098ab0d0fd1f52f1
Plesz Gábor <gabor.plesz@gmail.com>
2018-08-30 10:45:44 +0200

    #10: Naplózási feladat kezdőállapot

---
+++ 08Nap/07Serilog/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+
+namespace _07Serilog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+
+            while (!Console.KeyAvailable) //addig fut, amíg nem ütöttünk le billentyűt
+            {
+                //véletlenszerűen kiválasztunk egy szintet/súlyt a bejegyzéshez
+                var level = r.Next(100);
+
+                //szimuláljuk egy tetszőleges alkalmazás működését
+                //a kisebb súlyú bejegyzésekből többet szeretnénk, és minél súlyosabb egy bejegyzés, annál kevesebb legyen belőle
+
+                if (level < 50)
+                { //legkisebb súlyú, de legsűrűbben előforduló üzenet (Debug)
+
+                }
+
+                if (level >= 50 && level < 70)
+                { //Info
+
+                }
+
+                if (level >= 70 && level < 85)
+                { //Warning
+
+                }
+
+                if (level >= 85 && level < 95)
+                { //Error
+
+                }
+
+                if (level >= 95)
+                { //Fatal
+
+                }
+                Thread.Sleep(200);
+
+            }
+        }
+    }
+}
