3dd04f44f85122c7950895764ed7e346498b951c
Plesz Gábor <gabor.plesz@gmail.com>
2018-09-04 12:30:32 +0200

    #12: a függvénydefiníció kiváltása lambda kifejezéssel

--- 09Nap/11FuncActionLambda/Program.cs
+++ 09Nap/11FuncActionLambda/Program.cs
@@ -17,11 +17,37 @@ namespace _11FuncActionLambda
             SquaringDef squaringProcesslist = SquaringFunc; //(2.b)
 
             //nem ellenőrzüm a híváslista definiáltságát
-
             Console.WriteLine($"2 a négyzeten: {squaringProcesslist(2)}");
 
-            
+            //Feladat: hogy tudjuk ezt négy lépést csökkenteni???
 
+            //////////////////////////////
+            //1. csökkentő lépés: a lambda
+            //////////////////////////////
+
+            //lambda kifejezés használatával a 2.a/2.b lépést egy lépésre tudjuk csökkenteni
+
+            //a lambda kifejezés (=>) egy soron belüli függvénydefiníció
+            //a baloldalán a paraméterek listája van zárójelben, vesszővel elválasztva,
+            //a jobboldalán pedig a függvény törzse van.
+
+            //a függvény neve úgyis hivatkozásnak kellett, mivel a lambdát oda írjuk,
+            //ahol hivatkoznánk a függvény neve már nem is kell
+
+            //ha egy paraméter van, ÉS csak egy kifejezést használok akkor nem kellenek zárójelek () {}
+            squaringProcesslist = x => x * x;
+
+            //ugyanez kódblokkban:
+            squaringProcesslist = (x) => { return x * x; };
+            //kódblokkban akárhány sort használhatok már:
+
+            squaringProcesslist = (x) => 
+            {
+                Console.WriteLine($"négyzetre emelek, paraméter: {x}");
+                return x * x;
+            };
+
+            Console.WriteLine($"2 a négyzeten: {squaringProcesslist(2)}");
 
             Console.ReadLine();
         }
