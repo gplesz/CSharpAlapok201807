76109fb56fc3c735bd362f4e4dcd2d624dc3c2c8
Plesz Gábor <gabor.plesz@gmail.com>
2018-09-11 10:31:22 +0200

    #14: Path kezelése windows-on és linuxon

---
+++ 11Nap/01LocalData/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace _01LocalData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A Path a statikus osztály az elérési utakkal kapcsolatos dolgokban segít.
+
+            //általában érdemes tartózkodni az abszolút elérési utaktól, helyette relatív elérési utat érdemes használni.
+
+            //például: relatív elérési út
+            var path = "EgyikKonyvtar";
+
+            //és abszolút elérési út
+            Console.WriteLine($"Ennek a relatív könyvtárnak: {path}");
+            Console.WriteLine($" ez az abszolút elérési útja: {Path.GetFullPath(path)}");
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 11Nap/01LocalData/buildandrun.bat
@@ -0,0 +1,2 @@
+﻿docker build -t 01localdata .
+docker run 01localdata
