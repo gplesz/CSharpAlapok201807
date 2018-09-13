e14a8818f848a3658983c1c856af837884edc8fa
Plesz Gábor <gabor.plesz@gmail.com>
2018-09-13 10:32:33 +0200

    #15: adatok ellenőrzése és feltöltés, ha kell (code first)

--- 11Nap/03CodeFirstCore/Program.cs
+++ 11Nap/03CodeFirstCore/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using _03CodeFirstCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _03CodeFirstCore
 {
@@ -6,7 +10,38 @@ namespace _03CodeFirstCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var connectionString = "Server=.\\SQLEXPRESS;Database=CodeFirstDB;Trusted_Connection=True;";
+
+            var optionsBuilder = new DbContextOptionsBuilder<CodeFirstContext>();
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            var db = new CodeFirstContext(optionsBuilder.Options);
+
+            if (db.Teachers.Count() == 0)
+            { //adatok ellenőrzése
+                Seed(db);
+            }
+
+            Console.WriteLine($"A tanárok száma: {db.Teachers.Count()}, a tantárgyak száma: {db.Subjects.Count()}");
+
+            Console.ReadLine();
+
+        }
+
+        private static void Seed(CodeFirstContext db)
+        {
+            //todo: ellenőrizni kell az adatbázis létezését
+            //todo: és ha nem létezik, létre kell hozni.
+
+            var subjects = new List<Subject>();
+
+            subjects.Add(new Subject() { Name = "Matematika" });
+            subjects.Add(new Subject() { Name = "Fizika" });
+
+            db.Teachers.Add(new Teacher() { Name = "Matektanár", Subject = subjects });
+
+            db.SaveChanges();
         }
     }
 }
